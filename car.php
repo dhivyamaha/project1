80b128f170bdcbb3960f51c1aa994b39a58e5623
dhivyamaha <dhivyabharathi.b2012@kgkite.ac.in>
2016-03-07 16:04:06 +0530

    Added files via upload

---
+++ car.php
@@ -0,0 +1,25 @@
+<?php
+	mysql_connect("localhost","root","");
+	mysql_select_db("tollplaza");
+	
+	$name=$_REQUEST['name'];
+	$vehicleno=$_REQUEST['vehicleno'];
+	$passtype=$_REQUEST['passtype'];
+        $amount=$_REQUEST['amount'];
+	
+$SQL = "INSERT INTO `car`(`name`,`vehicleno`,`passtype`,`amount`,`status`) VALUES ('".$name."','".$vehicleno."','".$passtype."','".$amount."','not paid')";   
+	$RES = mysql_query($SQL); 
+$sql1 = "select id from car where vehicleno='".$vehicleno."' ";
+
+$result=mysql_query($sql1);
+while ($row = mysql_fetch_array($result))
+{
+	$r=$row['id']; 
+
+
+}
+echo $r;
+	  
+	
+	
+?>
